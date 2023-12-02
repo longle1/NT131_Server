a425ef89b5f0aa21cb39431fca88e102ec56f099
longle1 <ltphilong2001@gmail.com>
2023-12-02 18:53:43 +0700

    update send notification to android app

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,35 @@
 const express = require("express");
+var FCM = require('fcm-node');
 const admin = require('firebase-admin');
 const db = admin.database();
 const router = express.Router();
 const ref = db.ref('data');
+
+function sendNotification(keyApp) {
+  console.log("key app", keyApp)
+  var serverKey = 'AAAAsCaZLe0:APA91bGGgIxxtEcOOOGcrbX0jcNUGwttbbAeCsCRYTs2HziLdUZeH0cZQmKyBikh1PTvX9ZKSjVeSBykAP59BJfAuz1BcpiouSVBMevnzNE5FYpKYnOvDne983WgxPxpV2-xRr5EFHMi';
+    var fcm = new FCM(serverKey);
+
+    var message = {
+	    to:keyApp,
+        notification: {
+            title: 'Notification',
+            body: 'Your door is opened',
+      }
+    };
+
+    fcm.send(message, function(err, response) {
+        if (err) {
+            console.log("Something has gone wrong!"+err);
+			      console.log("Respponse:! "+response);
+        } else {
+            // showToast("Successfully sent with response");
+            console.log("Successfully sent with response: ", response);
+        }
+
+    });
+}
+
 router.get('/login', async (req, res) => {
   try {
     const {username, password} = req.query;
@@ -60,21 +87,21 @@ router.put('/status-door', async (req, res) => {
 
         res.status(200).json({
           "status": true,
-          "statusDoor": currentStatus
+          "data": currentStatus
         });
         
       } else {
         user = null; 
         res.status(400).json({
           "status": false,
-          "statusDoor": null
+          "data": null
         })
       }
     })
     .catch((error) => {
       res.status(400).json({
           "status": false,
-          "statusDoor": null
+          "data": null
         })
     });
   } catch (error) {
@@ -103,6 +130,7 @@ router.post('/check-password', async (req, res) => {
 
           let ps = userData['passwordDoor']; 
           if(ps == currentPs) {
+            sendNotification(userData['key']);
             res.status(201).json({
               "status": true,
               "message": "Correct password"
@@ -129,5 +157,50 @@ router.post('/check-password', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+// Check-password endpoint
+router.post('/face-recognition', async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    const usersRef = admin.database().ref('users');
+
+    const searchUsername = username; 
+
+    usersRef.orderByChild('username').equalTo(searchUsername).once('value')
+      .then((snapshot) => {
+        const userSnapshot = snapshot.val();
+        if (userSnapshot) {
+          const userKey = Object.keys(userSnapshot)[0];
+          const userData = userSnapshot[userKey];
+          if(userData) {
+            sendNotification(userData['key']);
+            res.status(201).json({
+              "status": true,
+              "message": "Success"
+            });
+          }else {
+            res.status(400).json({
+              "status": false,
+              "message": "Failed"
+            });
+          }
+        } else {
+          res.status(400).json({
+            "status": false,
+            "message": "User is not exist"
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error checking password:', error);
+      });
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 
 module.exports = router;
